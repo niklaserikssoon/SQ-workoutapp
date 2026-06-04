032d969bc247c1f2cde4d92b6a675e404974d61d
niklas eriksson <niklaseriksson32@hotmail.com>
2026-06-04 12:14:31 +0200

    change fautly config url

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  userApiUrl: "https://workoutapp-api.wonderfulpebble-02d3e465.italynorth.azurecontainerapps.io/",
+  userApiUrl: "https://user-api.wonderfulpebble-02d3e465.italynorth.azurecontainerapps.io/",
   workoutApiUrl: "https://workoutapp-api.wonderfulpebble-02d3e465.italynorth.azurecontainerapps.io/",
   exerciseListUrl: "https://raw.githubusercontent.com/yuhonas/free-exercise-db/master/dist/exercises.json"
 };
